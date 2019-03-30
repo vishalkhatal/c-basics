316cd573beb46ea1d3974efbec1e9fe52b44f6f2
Vishal Khatal <vishal.khatal2206@gmail.com>
2019-03-30 09:40:15 +0530

    Return Void vs Task

---
+++ AsyncMethodDemo/AsyncMethodDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMethod();
+            Console.ReadKey();
+        }
+
+        public static void TestMethod()
+        {
+            try
+            {
+                AsyncMethod();
+            }
+            catch (Exception ex)//Stmt inside this catch will never be executed.
+                                //Exception thrown by AsyncMethod will never be caught
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static async void AsyncMethod()
+        {
+            throw new Exception();
+        }
+    }
+}
