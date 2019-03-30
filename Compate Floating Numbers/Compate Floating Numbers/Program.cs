c6f6db4f11969b226daae73f9528b3c2a519fb4d
Vishal Khatal <vishal.khatal2206@gmail.com>
2019-03-30 09:48:52 +0530

    Compare float numbers

---
+++ Compate Floating Numbers/Compate Floating Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compate_Floating_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myNumber = 3.146f;
+
+            if (myNumber.IsApproximately(3.146f, within: 0.001f))
+            {
+                Console.Write("Numbers matching");
+            }
+            else
+            {
+                Console.Write("Numbers not matching");
+            }
+        }
+    }
+    public static class FloatAndDoubleExt
+    {
+        public static bool IsApproximately(this double self, double other, double within)
+        {
+            return Math.Abs(self - other) <= within;
+        }
+
+        public static bool IsApproximately(this float self, float other, float within)
+        {
+            return Math.Abs(self - other) <= within;
+        }
+    }
+}
